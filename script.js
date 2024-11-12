368e24e64921f3c91f58bddca8d82b62205f274f
Daniel Oshima <doshima2013@gmail.com>
2024-11-12 09:38:58 -0500

    reverted to previous mouseover event code but moved <script> to end of body of html page

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ h2.textContent = "Welcome to Daniel Oshima's first Website";
 
 document.querySelector("body").appendChild(h2);
 
-document.addEventListener('DOMContentLoaded', function() {
-    const input = document.getElementById('image');
-    input.addEventListener('mouseover', function() {
-      alert('This is an image of me!');
-    });
-  });
+const input = document.getElementById('image');
+input.addEventListener('mouseover', function() {
+  alert('This is an image of me!');
+});
+
