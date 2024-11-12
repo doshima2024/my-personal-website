86beac28bcba0e95ae2fff061a7c632e0d944abf
Daniel Oshima <doshima2013@gmail.com>
2024-11-12 08:40:22 -0500

    Failed attempt at creating a mouseover alert using JS

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const h2 = document.createElement("h2")
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Welcome to Daniel Oshima's first Website";
 
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById('image');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
